2a1d2bae852885ec305aa60c3a6391d08363f70d
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-01-20 16:57:05 +0800

    add

--- src/components/Picture/index.tsx
+++ src/components/Picture/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import "./index.less";
-import "./animate.less";
+import "../../style/css/animate.less";
 interface IProps {
   src?: string;
   name?: string;
--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,7 @@
 
 import("./index");
 import("./themes/index.less");
+import("./style/css/animate.less");
 import("./components/Header/index.less");
 import("./components/Picture/index.less");
 import("./components/PictureList/index.less");
